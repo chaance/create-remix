764dbe087ae13d7eab4c323a6072a58615c33825
Chance Strickland <hi@chance.dev>
2023-02-14 16:53:27 +0100

    handle http errors when requesting version from npm

--- src/remix-version.ts
+++ src/remix-version.ts
@@ -5,14 +5,28 @@ export async function getLatestRemixVersion() {
 		if (_versionCache) {
 			return resolve(_versionCache);
 		}
-		https.get("https://registry.npmjs.org/remix/latest", (res) => {
-			let body = "";
-			res.on("data", (chunk) => (body += chunk));
-			res.on("end", () => {
-				let { version } = JSON.parse(body);
-				_versionCache = version;
-				resolve(version);
-			});
+		let request = https.get(
+			"https://registry.npmjs.org/remix/latest",
+			(res) => {
+				let body = "";
+				res.on("data", (chunk) => (body += chunk));
+				res.on("end", () => {
+					let { version } = JSON.parse(body);
+					_versionCache = version;
+					resolve(version);
+				});
+			}
+		);
+
+		// set a short timeout to avoid super slow initiation
+		request.setTimeout(5000);
+
+		// This avoids annoying connection reset issues as a result of a closed
+		// connection at the end, after we already have what we need from our get
+		// request to npm. If the request fails entirely we'll just hard-code a
+		// value and try to keep it updated.
+		request.on("error", () => {
+			resolve(_versionCache || "1.12.0");
 		});
 	});
 }
